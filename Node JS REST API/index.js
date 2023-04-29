54bca363dbb8d763cb70ddfc206ec4089ff8bf37
kieransahoo <kkieran50@gmail.com>
2023-04-29 19:36:08 +0530

    first node app

---
+++ Node JS REST API/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import usersRoutes from './routes/users.js';
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+// all routes in here are starts from /users
+app.use('/users',usersRoutes);
+
+// checking for get request 
+app.get('/',(req,res) =>res.send('Hello from Homepage.'));
+
+app.listen(PORT,()=> console.log(`Server running on port: http://localhost:${PORT}`));
